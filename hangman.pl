2edcac6233e06af4a8683ed1ec5635d3c77c65c9
AwrenNuit <a.rennaker@yahoo.com>
2020-08-15 16:35:18 -0500

    updated word lists

--- hangman.pl
+++ hangman.pl
@@ -8,6 +8,7 @@ my $EOL  = "\n";
 my $EMPTY_STRING = q{};
 
 my $menuAnswer   = $EMPTY_STRING;
+my $difficulty   = $EMPTY_STRING;
 my $playerGuess  = $EMPTY_STRING;
 my $selectedWord = $EMPTY_STRING;
 
@@ -15,13 +16,98 @@ my @guessedLetters        = ('1');
 my @selectedWordDisplay   = ();
 my $wrongGuessesRemaining = 6;
 
-my @wordList = qw(
-  HORSE          PUMPKIN        COMPUTER 
-  SASSAFRASS     LYCANTHROPES   HANGMAN 
-  FLAMETHROWER   XYLOPHONE      EXCOMMUNICATION 
-  UTOPIA         BRIEFCASE      DOCTOR 
-  MASTERPIECE    BOOKKEEPER     EMPHASIZE
+my @activeWordList = qw();
+
+
+
+
+
+
+
+
+
+##########################################################
+#       WORD LISTS - SPOILERS! SPOILERS! SPOILERS!       #
+##########################################################
+
+my @easierWordList = qw (
+  ACROSS     ATTACK      BASKET 
+  BETTER     BOTTLE      CARRY 
+  CAT        CERTAIN     CHAIR 
+  CHOCOLATE  CHOOSE      CIRCLE 
+  CLEAN      CLOTHES     COLOR 
+  CORRECT    COUNT       EARTH 
+  ELECTRIC   EVENING     FALSE 
+  FLOOR      FOOTBALL    FRIEND 
+  GATE       GOODBYE     GRASS 
+  GREEN      HAPPEN      HEALTH 
+  HEART      HEIGHT      HELLO 
+  HORSE      HOTEL       IMPORTANT 
+  INSIDE     IRON        ISLAND 
+  JUST       KITCHEN     LADDER 
+  LATELY     LEARN       LEFT 
+  LENGTH     LESSON      LETTER 
+  LIBRARY    LISTEN      LITTLE 
+  LONELY     LOOK        MARKET 
+  MATTER     MEAT        MILLION 
+  MOMENT     MONTH       NATURE 
+  NEEDLE     NEITHER     NOISE 
+  NOSE       NOTHING     ORDER 
+  OUTSIDE    PARENT      PARTNER 
+  PART       PAST        PATH 
+  PEOPLE     PHOTOGRAPH  PICTURE 
+  PLASTIC    PLEASED     POISON 
+  POLITE     POTATO      PRESENT 
+  PRETTY     PROTECT     QUESTION 
+  RECEIVE    RESTAURANT  RESULT 
+  RUBBER     RULER       SALT 
+  SENTENCE   SILLY       SIMILAR 
+  SMALL      SMELL       SMILE 
+  SOMEONE    SOMETHING   SOMETIMES 
+  SPEED      SPELL       SPOON 
+  SPRING     STEAL       STEAM 
+  STONE      STOP        STORE 
+  STREET     STUDENT     SUCCESS 
+  TABLE      TASTE       TELEPHONE 
+  TENNIS     TERRIBLE    THAT 
+  THREE      TITLE       TOGETHER 
+  TOMORROW   TONIGHT     TOOL 
+  TOOTH      TOTAL       TREE 
+  TRUE       TRUST       USUAL 
 );
+
+my @harderWordList = qw(
+  AMBIDEXTROUS     BANDWAGON       BLITZ 
+  BLIZZARD         BAYOU           EXCOMMUNICATION 
+  GALVANIZE        GNARLY          HAPHAZARD 
+  JOVIAL           KHAKI           KILOBYTE 
+  LARYNX           LYCANTHROPES    MALNOURISHED 
+  MEGAHERTZ        METALWORKING    ONYX 
+  PIZAZZ           PROBLEMATIC     PSEUDOMYTHICAL 
+  QUESTIONABLY     QUIXOTIC        SPHINX 
+  SUBVOCALISED     SYNDROME        TWELFTHS 
+  UNCOPYRIGHTABLE  UNDISCOVERABLE  UTOPIA 
+  VOYEURISM        WALTZ           XYLOPHONE 
+);
+
+my @extraWordList = qw(
+  BOOKKEEPER  BRIEFCASE    COMPUTER 
+  DOCTOR      EMPHASIZE    FLAMETHROWER 
+  HANGMAN     MASTERPIECE  PUMPKIN 
+  SASSAFRASS  SEISMIC      TAPESTRY 
+);
+
+
+
+
+
+
+
+
+
+##########################
+#       START GAME       #
+##########################
 
 mainMenu();
 
@@ -30,9 +116,9 @@ sub attemptToSolve {
   my $solutionGuess = <STDIN>;
   chomp($solutionGuess);
   if ( $selectedWord eq uc($solutionGuess) ) {
-    print 'Congratlutions. You have bested me and saved a life.' . $EOL x2;
+    print $EOL . 'Congratlutions. You have bested me and saved a life.' . $EOL x2;
     sleep(1);
-    print 'Goodbye.';
+    print 'Goodbye.' . $EOL x2;
   } else {
     handleGuessesRemaining();
     guessLetter();
@@ -63,26 +149,68 @@ sub checkGuessedLetter {
   guessLetter();
 }
 
-sub guessLetter {
-  if ( $wrongGuessesRemaining == 0 ) {
-    print 'You have lost. Another life has been taken.' . $EOL x2;
+sub checkIfSolved {
+  my $checkSelectedWord = join('', @selectedWordDisplay);
+  $checkSelectedWord =~ s/\s+//g;
+  if ( $selectedWord eq $checkSelectedWord ) {
+    print $EOL . $EOL . 'Congratlutions. You have bested me and saved a life.' . $EOL x2;
     sleep(1);
-    print 'Goodbye.';
-    return;
+    print 'Goodbye.' . $EOL x2;
+    return 0;
   } else {
-    if ( scalar @guessedLetters == 1 ) {
-      print 'What is your guess?' . $EOL;
+    return 1;
+  }
+}
+
+sub chooseDifficulty {
+  print $EOL . 'Which difficulty do you choose? EASY/HARD/ALL' . $EOL;
+  $difficulty = <STDIN>;
+  chomp($difficulty);
+  if ( uc($difficulty) eq 'EASY' ) {
+    push(@activeWordList, @easierWordList);
+    sleep(1);
+    print $EOL . 'You have chosen EASY.' . $EOL;
+  } elsif ( uc($difficulty) eq 'HARD' ) {
+    push(@activeWordList, @harderWordList);
+    sleep(1);
+    print $EOL . 'You have chosen HARD.' . $EOL;
+  } elsif ( uc($difficulty) eq 'ALL' ) {
+    push(@activeWordList, @easierWordList);
+    push(@activeWordList, @harderWordList);
+    push(@activeWordList, @extraWordList);
+    sleep(1);
+    print $EOL . 'You have chosen ALL.' . $EOL;
+  } else {
+    sleep(1);
+    print $EOL . 'I ask again...';
+    chooseDifficulty();
+  }
+}
+
+sub guessLetter {
+  if ( checkIfSolved() ) {
+    if ( $wrongGuessesRemaining == 0 ) {
+      print 'You have lost. Another life has been taken.' . $EOL x2;
+      sleep(1);
+      print 'Goodbye.' . $EOL x2;
+      return;
     } else {
-      print 'What is your next guess? (type SOLVE to solve)' . $EOL;
+      if ( scalar @guessedLetters == 1 ) {
+        print 'What is your guess?' . $EOL;
+      } else {
+        print 'What is your next guess? (type SOLVE to solve)' . $EOL;
+      }
+      $playerGuess = <STDIN>;
+      chomp($playerGuess);
+      $playerGuess = uc($playerGuess);
+      if ( $playerGuess eq 'SOLVE' ) {
+        attemptToSolve();
+      } else {
+        checkGuessedLetter();
+      }
     }
-    $playerGuess = <STDIN>;
-    chomp($playerGuess);
-    $playerGuess = uc($playerGuess);
-    if ( $playerGuess eq 'SOLVE' ) {
-      attemptToSolve();
-    } else {
-      checkGuessedLetter();
-    }
+  } else {
+    return;
   }
 }
 
@@ -107,14 +235,17 @@ sub mainMenu {
   chomp($menuAnswer);
   if ( uc($menuAnswer) eq 'YES' ) {
     sleep(1);
+    chooseDifficulty();
+    sleep(1);
     print $EOL . 'Then the game shall begin.';
     sleep(1);
     startGame();
   } elsif ( uc($menuAnswer) eq 'NO' ) {
-    print 'Goodbye.';
+    sleep(1);
+    print $EOL . 'Goodbye.' . $EOL x2;
   } else {
     sleep(1);
-    print 'I ask again...';
+    print $EOL . 'I ask again...';
     mainMenu();
   }
 }
@@ -128,7 +259,7 @@ sub printDisplay {
 }
 
 sub selectWord {
-  $selectedWord = $wordList[rand @wordList];
+  $selectedWord = $activeWordList[rand @activeWordList];
   setDisplay();
 }
 
